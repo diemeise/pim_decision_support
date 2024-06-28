2bc998a505107ea07875bd71675792d41abf1f89
Simon <simon@desktop.de>
2024-06-28 20:42:20 +0200

    fix array syntax in rules

--- rules.js
+++ rules.js
@@ -91,7 +91,7 @@ const rules = [
   },
   {
     "name": "Letzte Kontrolle ueberpruefen",
-    "condition": "questionnaire.ths_batterie_aelter_drei_Monate === true || \"entzugssymptome\" in questionnaire.parkinson_symptome_aktuell_symptome || questionnaire.schrittmacher_erschoepft === true",
+    "condition": "questionnaire.ths_batterie_aelter_drei_Monate === true || questionnaire.parkinson_symptome_aktuell_symptome.includes(\"entzugssymptome\") || questionnaire.schrittmacher_erschoepft === true",
     "action": "goto=Schrittmacher ueberpruefen",
     "else": "goto=Keine Empfehlung moeglich"
   },
