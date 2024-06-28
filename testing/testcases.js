fd04ac5784a532e37098fa2d61a036003c27181d
Simon <simon@desktop.de>
2024-06-28 20:52:46 +0200

    add one more testcase

--- testing/testcases.js
+++ testing/testcases.js
@@ -1,7 +1,12 @@
 function loadTestCases(){
     return[case1 = {
         questionnaire: testcase1,
-        expected: expected1
+        expected: expected1   
+    },
+    case2 = {
+        questionnaire: testcase2,
+        expected: expected2,
+        
     }]
 }
 
@@ -28,7 +33,7 @@ const testcase1 = {
     "pk_therapie_vergangenheit": true,
     "pumpentherapie_moeglich": true,
     "risiko_gegen_op": true,
-    "schrittmacher_erschoepft": true,
+    "schrittmacher_hat_Ladung": true,
     "schwerer_tremor_vorhanden": true,
     "symptome_nach_medikamente_vorhanden": true,
     "ths_batterie_aelter_drei_Monate": true,
@@ -39,6 +44,38 @@ const testcase1 = {
 }
 const expected1 = "Beendet: Batterie des Schrittmachers ueberpruefen"
 
-
+const testcase2 = {
+    "stunde_ueberbeweglichkeit": true,
+    "5_medikamente_tag": true,
+    "THS_moeglich": true,
+    "aktuelle_medikamente_sind_ausreichend": true,
+    "aktuelle_medikamente_sind_effektiv": true,
+    "aktuelle_therapieform": "THS",
+    "alter_bei_PK_diagnose": "12",
+    "impulskontrollstoerung_durch_medikamente": true,
+    "impulskontrollstoerung_vorhanden": true,
+    "kontraindikation_STN": true,
+    "motorische_fluktuationen_durch_levodopa_kontrolliert": true,
+    "motorische_fluktuationen_weniger_3_jahre": true,
+    "off_symptome_2h": true,
+    "orale_medikamente_moeglich": true,
+    "parkinson_symptome_aktuell": true,
+    "parkinson_symptome_aktuell_symptome": [],
+    "patient_alter": "24",
+    "pk_diagnose_vorhanden": true,
+    "pk_therapie_aktuell": true,
+    "pk_therapie_vergangenheit": true,
+    "pumpentherapie_moeglich": true,
+    "risiko_gegen_op": true,
+    "schrittmacher_hat_Ladung": true,
+    "schwerer_tremor_vorhanden": true,
+    "symptome_nach_medikamente_vorhanden": true,
+    "ths_batterie_aelter_drei_Monate": false,
+    "ths_batterie_geladen": true,
+    "ths_erfolgreich": true,
+    "verbesserung_durch_optimierung_oraler_therapie": true,
+    "wirkungsfluktationen_vorhanden": true
+}
+const expected2 = "Beendet: Batterie des Schrittmachers ueberpruefen"
 
 setDebugButtons();
